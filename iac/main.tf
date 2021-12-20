0740caa8c0b20c0278b249d132cef86de88e7d5a
Jason Spangler <9028535+jaspangler@users.noreply.github.com>
2021-12-19 19:00:29 -0500

    reverted back

--- iac/main.tf
+++ iac/main.tf
@@ -15,7 +15,7 @@ provider "azurerm" {
 }
 
 # Create a resource group
-resource "azurerm_resource_group" "hellow-world-two" {
+resource "azurerm_resource_group" "example" {
   name     = "hello-world-two"
   location = "East US 2"
 }
