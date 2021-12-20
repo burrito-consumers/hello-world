1901261401921743a869c790640793b0a5d775ef
Jason Spangler <9028535+jaspangler@users.noreply.github.com>
2021-12-19 19:47:42 -0500

    avoiding name conflict

--- iac/cdn.tf
+++ iac/cdn.tf
@@ -6,13 +6,13 @@ resource "azurerm_cdn_profile" "cdn" {
 }
 
 resource "azurerm_cdn_endpoint" "hello-world" {
-  name                = "hello-world"
+  name                = "hello-world-two"
   profile_name        = azurerm_cdn_profile.cdn.name
   location            = azurerm_resource_group.hello-world.location
   resource_group_name = azurerm_resource_group.hello-world.name
 
   origin {
-    name      = "hello-world"
+    name      = "hello-world-two"
     host_name = "hello-world.spanglab.com"
   }
 }
