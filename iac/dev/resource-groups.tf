e158daba6484671d33697f9560c17ff02f183036
Jason Spangler <9028535+jaspangler@users.noreply.github.com>
2021-12-20 20:04:41 -0500

    added another resource group as example

--- iac/dev/resource-groups.tf
+++ iac/dev/resource-groups.tf
@@ -3,3 +3,8 @@ resource "azurerm_resource_group" "hello-world" {
   name     = "hello-world-three"
   location = "East US 2"
 }
+
+resource "azurerm_resource_group" "hello-world-four" {
+  name     = "hello-world-four"
+  location = "East US 2"
+}
